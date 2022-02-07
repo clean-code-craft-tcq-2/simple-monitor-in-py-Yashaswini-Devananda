bf3a00178aa8eb0d7e472d0ff6373407a3da12d5
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-02-07 23:23:35 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -24,7 +24,7 @@ min_temp_check = Battery_param_limit_test_data(B1.battery_temp.min_permissible_v
 beyond_max_temp_check = Battery_param_limit_test_data((B1.battery_temp.max_permissible_value + 0.1) , B1.battery_soc.max_permissible_value, B1.battery_charge_rate.max_permissible_value)
 beyond_min_temp_check = Battery_param_limit_test_data((B1.battery_temp.min_permissible_value - 0.1) , B1.battery_soc.max_permissible_value, B1.battery_charge_rate.max_permissible_value)
 in_range_min_temp_check = Battery_param_limit_test_data((B1.battery_temp.min_permissible_value + 0.1) , B1.battery_soc.max_permissible_value, B1.battery_charge_rate.max_permissible_value)
-in_range_max_temp_check = Battery_param_limit_test_data((B1.battery_temp.max_permissible_value + 0.1) , B1.battery_soc.max_permissible_value, B1.battery_charge_rate.max_permissible_value)
+in_range_max_temp_check = Battery_param_limit_test_data((B1.battery_temp.max_permissible_value - 0.1) , B1.battery_soc.max_permissible_value, B1.battery_charge_rate.max_permissible_value)
 
 if __name__ == '__main__':
     assert(battery_is_ok(max_temp_check) is True)
