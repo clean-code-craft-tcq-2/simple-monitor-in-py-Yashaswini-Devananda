5e64e851d7e84935c5947e1b1583e44864d3006e
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-02-07 22:27:09 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,18 +1,3 @@
-# class BMSParameter:
-#     min_permissible_value = 0
-#     max_permissible_value = 0
-
-# battery_temp = BMSParameter()
-# battery_temp.min_permissible_value = 0
-# battery_temp.max_permissible_value = 45
-
-# battery_soc = BMSParameter()
-# battery_soc.min_permissible_value = 20
-# battery_soc.max_permissible_value = 80
-
-# battery_charge_rate = BMSParameter()
-# battery_charge_rate.min_permissible_value = 0
-# battery_charge_rate.max_permissible_value = 0.8
 import battery_param_permssible_range as B1
 
 def isValueNotInRange(parameter_value, min_value, max_value):
