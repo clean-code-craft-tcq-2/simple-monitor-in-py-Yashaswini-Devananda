8caf56f8db19f3c6ce4b1f7be2e5e48f95c62904
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-02-07 22:25:14 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,34 +1,31 @@
-class BMSParameter:
-    min_permissible_value = 0
-    max_permissible_value = 0
+# class BMSParameter:
+#     min_permissible_value = 0
+#     max_permissible_value = 0
 
-battery_temp = BMSParameter()
-battery_temp.min_permissible_value = 0
-battery_temp.max_permissible_value = 45
+# battery_temp = BMSParameter()
+# battery_temp.min_permissible_value = 0
+# battery_temp.max_permissible_value = 45
 
-battery_soc = BMSParameter()
-battery_soc.min_permissible_value = 20
-battery_soc.max_permissible_value = 80
+# battery_soc = BMSParameter()
+# battery_soc.min_permissible_value = 20
+# battery_soc.max_permissible_value = 80
 
-battery_charge_rate = BMSParameter()
-battery_charge_rate.min_permissible_value = 0
-battery_charge_rate.max_permissible_value = 0.8
+# battery_charge_rate = BMSParameter()
+# battery_charge_rate.min_permissible_value = 0
+# battery_charge_rate.max_permissible_value = 0.8
+import battery_param_permssible_range as B1
 
 def isValueNotInRange(parameter_value, min_value, max_value):
     return parameter_value < min_value or parameter_value > max_value # returns true if any 1 con is true
 
 def battery_is_ok(temperature, soc, charge_rate):
-    temperatureNotOk = isValueNotInRange(temperature, battery_temp.min_permissible_value, battery_temp.max_permissible_value)
-    socNotOk = isValueNotInRange(soc, battery_soc.min_permissible_value, battery_soc.max_permissible_value)
-    charge_rateNotOk = isValueNotInRange(charge_rate, battery_charge_rate.min_permissible_value, battery_charge_rate.max_permissible_value)
+    temperatureNotOk = isValueNotInRange(temperature, B1.battery_temp.min_permissible_value, B1.battery_temp.max_permissible_value)
+    socNotOk = isValueNotInRange(soc, B1.battery_soc.min_permissible_value, B1.battery_soc.max_permissible_value)
+    charge_rateNotOk = isValueNotInRange(charge_rate, B1.battery_charge_rate.min_permissible_value, B1.battery_charge_rate.max_permissible_value)
     return not(temperatureNotOk or socNotOk or charge_rateNotOk)
 #     if temperatureNotOk or socNotOk or charge_rateNotOk:
 #         return False
 #     return True
-if battery_temp.min_permissible_value == battery_temp.max_permissible_value:
-    print ("No")
-else:
-    print ("Yes")  
 
 if __name__ == '__main__':
   assert(battery_is_ok(25, 70, 0.7) is True)
