4e9610d2e71b220309ec94b385fdda75a0d03a1f
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-02-07 23:26:27 +0530

    Update battery_param_permssible_range.py

--- battery_param_permssible_range.py
+++ battery_param_permssible_range.py
@@ -1,9 +1,9 @@
-class BMSParameter:
+class BatteryParameter:
     def __init__(self, min_permissible_value, max_permissible_value):
         self.min_permissible_value = min_permissible_value
         self.max_permissible_value = max_permissible_value
 
-battery_temp = BMSParameter(0, 45)
-battery_soc = BMSParameter(20, 80)
-battery_charge_rate = BMSParameter(0, 0.8)
+battery_temp = BatteryParameter(0, 45)
+battery_soc = BatteryParameter(20, 80)
+battery_charge_rate = BatteryParameter(0, 0.8)
 
