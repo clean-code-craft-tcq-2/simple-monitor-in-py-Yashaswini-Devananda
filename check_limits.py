c648e99076864ca394f4d0d1948f8bc524ecf695
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-02-07 23:12:00 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -19,9 +19,11 @@ class Battery_param_limit_test_data:
     self.soc = soc
     self.charge_rate = charge_rate
   
-test_data1 = Battery_param_limit_test_data(B1.battery_temp.max_permissible_value , B1.battery_soc.max_permissible_value, B1.battery_charge_rate.max_permissible_value)
+max_temp_check = Battery_param_limit_test_data(B1.battery_temp.max_permissible_value , B1.battery_soc.max_permissible_value, B1.battery_charge_rate.max_permissible_value)
+min_temp_check = Battery_param_limit_test_data(B1.battery_temp.min_permissible_value , B1.battery_soc.max_permissible_value, B1.battery_charge_rate.max_permissible_value)
+beyond_max_temp_check = Battery_param_limit_test_data((B1.battery_temp.max_permissible_value + 0.1_ , B1.battery_soc.max_permissible_value, B1.battery_charge_rate.max_permissible_value)
 
 if __name__ == '__main__':
-  assert(battery_is_ok(test_data1) is True)
+  assert(battery_is_ok(beyond_max_temp_check) is False)
 #   assert(battery_is_ok(25, 70, 0.7) is True)
 #   assert(battery_is_ok(50, 85, 0) is False)
