b107f45656005ecfb8f3e607b1e456e03fc3832c
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-02-07 21:25:28 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,16 +1,30 @@
+class BMSParameter:
+    min_permissible_value = 0
+    max_permissible_value = 0
+
+battery_temp = BMSParameter()
+battery_temp.min_permissible_value = 0
+battery_temp.max_permissible_value = 45
+
+battery_soc = BMSParameter()
+battery_soc.min_permissible_value = 20
+battery_soc.max_permissible_value = 80
+
+battery_charge_rate = BMSParameter()
+battery_charge_rate.min_permissible_value = 0
+battery_charge_rate.max_permissible_value = 0.8
+
+def isValueNotInRange(parameter_value, min_value, max_value):
+    return parameter_value < min_value or parameter_value > max_value # returns true if any 1 con is true
 
 def battery_is_ok(temperature, soc, charge_rate):
-  if temperature < 0 or temperature > 45:
-    print('Temperature is out of range!')
-    return False
-  elif soc < 20 or soc > 80:
-    print('State of Charge is out of range!')
-    return False
-  elif charge_rate > 0.8:
-    print('Charge rate is out of range!')
-    return False
-
-  return True
+    temperatureNotOk = isValueNotInRange(temperature, battery_temp.min_permissible_value, battery_temp.max_permissible_value)
+    socNotOk = isValueNotInRange(soc, battery_soc.min_permissible_value, battery_soc.max_permissible_value)
+    charge_rateNotOk = isValueNotInRange(charge_rate, battery_charge_rate.min_permissible_value, battery_charge_rate.max_permissible_value)
+    if temperatureNotOk or socNotOk or charge_rateNotOk:
+        return False
+    else:
+        return True
 
 
 if __name__ == '__main__':
