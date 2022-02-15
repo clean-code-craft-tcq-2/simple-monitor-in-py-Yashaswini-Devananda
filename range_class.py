1026b591f94398cd5118f94da9ec612e74b87282
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-02-16 01:28:46 +0530

    Update range_class.py

--- range_class.py
+++ range_class.py
@@ -17,14 +17,13 @@ class Range:
  
         
     def isValueNotInRange(self, parameter_value):
+        tolerance_value = (self.tolerance/100)*self.max
         if parameter_value < self.min:
             return Status_message.LOW_BREACH
         elif parameter_value > self.max:
             return Status_message.HIGH_BREACH
-        elif self.warning_flag == True:
-            tolerance_value = (self.tolerance/100)*self.max
-            if parameter_value <= (self.min+tolerance_value):
-                return Status_message.LOW_WARNING
-            elif parameter_value >= (self.max-tolerance_value):
-                return Status_message.HIGH_WARNING 
+        elif (self.warning_flag == True) and (parameter_value <= (self.min+tolerance_value)):
+            return Status_message.LOW_WARNING
+        elif (self.warning_flag == True) and (parameter_value >= (self.max-tolerance_value)):
+            return Status_message.HIGH_WARNING 
         return Status_message.NORMAL
