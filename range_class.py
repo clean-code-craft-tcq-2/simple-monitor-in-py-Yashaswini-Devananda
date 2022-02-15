644b2b09024f14ea46682d5b826f9b9a00c07e3b
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-02-16 01:17:02 +0530

    Update range_class.py

--- range_class.py
+++ range_class.py
@@ -1,7 +1,21 @@
-class Range:
-    def __init__(self, min, max):
-        self.min = min
-        self.max = max
+import enum
 
+class Range:
+    def __init__(self, **kwargs):
+        self.min = kwargs.get('min', 0)
+        self.max = kwargs.get('max', 0)
+        self.warning_flag = kwargs.get('warning_flag', False)
+        self.tolerance = kwargs.get('tolerance', 0)
+        
     def isValueNotInRange(self, parameter_value):
-        return parameter_value < self.min or parameter_value > self.max # returns true if any 1 con is true
+        if parameter_value < self.min:
+            return Status_message.LOW_BREACH
+        elif parameter_value > self.max:
+            return Status_message.HIGH_BREACH
+        elif self.warning_flag == True:
+            tolerance_value = (self.tolerance/100)*self.max
+            if parameter_value <= (self.min+tolerance_value):
+                return Status_message.LOW_WARNING
+            elif parameter_value >= (self.max-tolerance_value):
+                return Status_message.HIGH_WARNING 
+        return Status_message.NORMAL
