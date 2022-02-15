acbee09427e39f0f975c812e2f3848c0e42848fb
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-02-16 01:23:19 +0530

    Update range_class.py

--- range_class.py
+++ range_class.py
@@ -1,11 +1,20 @@
 import enum
 
+class Status_message(enum.IntEnum):
+    LOW_BREACH = 0
+    HIGH_BREACH = 1
+    NORMAL = 2
+    LOW_WARNING = 3
+    HIGH_WARNING = 4
+    
+    
 class Range:
     def __init__(self, **kwargs):
         self.min = kwargs.get('min', 0)
         self.max = kwargs.get('max', 0)
         self.warning_flag = kwargs.get('warning_flag', False)
         self.tolerance = kwargs.get('tolerance', 0)
+ 
         
     def isValueNotInRange(self, parameter_value):
         if parameter_value < self.min:
